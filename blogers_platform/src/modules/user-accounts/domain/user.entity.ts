b5522acfcdc611368f2897ae53dadcb72ae7d431
H4w3er <mr.gavrik378@list.ru>
2025-06-19 16:39:59 +0300

    created main user schema and module, also created a few methods in class User

---
+++ blogers_platform/src/modules/user-accounts/domain/user.entity.ts
@@ -0,0 +1,78 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Error, HydratedDocument, Model } from 'mongoose';
+import { UpdateUserDto } from '../dto/create-user.dto';
+import { CreateUserDomainDto } from './dto/create-user.domain.dto';
+import { Name, NameSchema } from './name.schema';
+
+//флаг timestemp автоматичеки добавляет поля upatedAt и createdAt
+
+@Schema({ timestamps: true })
+export class User {
+
+  @Prop({ type: String, required: true })
+  login: string;
+
+  @Prop({ type: String, required: true })
+  passwordHash: string;
+
+  @Prop({ type: String, min: 5, required: true })
+  email: string;
+
+  @Prop({ type: Boolean, required: true, default: false })
+  isEmailConfirmed: boolean;
+
+  // @Prop(NameSchema) this variant from doc doesn't make validation for inner object
+  @Prop({ type: NameSchema })
+  name: Name;
+
+  createdAt: Date;
+  updatedAt: Date;
+
+  @Prop({ type: Date, nullable: true })
+  deletedAt: Date | null;
+
+  get id() {
+    // @ts-ignore
+    return this._id.toString();
+  }
+
+  static createInstance(dto: CreateUserDomainDto): UserDocument {
+    const user = new this();
+    user.email = dto.email;
+    user.passwordHash = dto.passwordHash;
+    user.login = dto.login;
+    user.isEmailConfirmed = false; // пользователь ВСЕГДА должен после регистрации подтверждить свой Email
+
+    user.name = {
+      firstName: 'firstName xxx',
+      lastName: 'lastName yyy',
+    };
+    user.deletedAt = null;
+    return user as UserDocument;
+  }
+
+  makeDeleted() {
+    if (this.deletedAt !== null) {
+      throw new Error('Entity already deleted');
+    }
+    this.deletedAt = new Date();
+  }
+
+  update(dto: UpdateUserDto) {
+    if (dto.email !== this.email) {
+      this.isEmailConfirmed = false;
+      this.email = dto.email;
+    }
+  }
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+//регистрирует методы сущности в схеме
+UserSchema.loadClass(User);
+
+//Типизация документа
+export type UserDocument = HydratedDocument<User>;
+
+//Типизация модели + статические методы
+export type UserModelType = Model<UserDocument> & typeof User;

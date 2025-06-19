eaaef6d5f240007a79cb90474eedef79726caea8
H4w3er <mr.gavrik378@list.ru>
2025-06-19 16:29:30 +0300

    added imports for UsersAccountsModule and root for mongoose

--- blogers_platform/src/app.module.ts
+++ blogers_platform/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersAccountsModule } from './modules/user-accounts/users-accounts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://artem:admin@data.xluig.mongodb.net/dev?retryWrites=true&w=majority&appName=Data`), // Укажите свой URL MongoDB
+    UsersAccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

c7b7a70e7fde3583d469cb0ed0fb9e7c00211160
H4w3er <mr.gavrik378@list.ru>
2025-06-25 22:32:10 +0300

    added update and get endpoints

--- blogers_platform/src/modules/blogs-posts-comments/blogs/api/blogs.controller.ts
+++ blogers_platform/src/modules/blogs-posts-comments/blogs/api/blogs.controller.ts
@@ -1,12 +1,24 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import { BlogsService } from '../application/blogs.service';
-import { GetBlogsQueryParams } from './input-dto/get-blogs-query-params.input-dto';
-import { PaginatedViewDto } from '../../../../core/dto/base.paginated.view-dto';
-import { BlogsQueryRepository } from '../infrastructure/blogs.query-repository';
-import { BlogViewDto } from './view-dto/blogs.view-dto';
-import { CreateBlogInputDto } from './input-dto/blogs.input-dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from "@nestjs/common";
+import { BlogsService } from "../application/blogs.service";
+import { GetBlogsQueryParams } from "./input-dto/get-blogs-query-params.input-dto";
+import { PaginatedViewDto } from "../../../../core/dto/base.paginated.view-dto";
+import { BlogsQueryRepository } from "../infrastructure/blogs.query-repository";
+import { BlogViewDto } from "./view-dto/blogs.view-dto";
+import { CreateBlogInputDto } from "./input-dto/blogs.input-dto";
+import { UpdateBlogDto } from "../dto/create-blog.dto";
 
-@Controller('blogs')
+@Controller("blogs")
 export class BlogsController {
   constructor(
     private blogsService: BlogsService,
@@ -14,24 +26,35 @@ export class BlogsController {
   ) {}
 
   @Get()
-  async getAll(@Query() query: GetBlogsQueryParams): Promise<PaginatedViewDto<BlogViewDto[]>> {
+  async getAll(
+    @Query() query: GetBlogsQueryParams,
+  ): Promise<PaginatedViewDto<BlogViewDto[]>> {
     return this.blogsQueryRepository.getAll(query);
   }
 
   @Post()
-  async createUser(@Body() body: CreateBlogInputDto): Promise<BlogViewDto> {
+  async createBlog(@Body() body: CreateBlogInputDto): Promise<BlogViewDto> {
     const blogId = await this.blogsService.createBlog(body);
     return this.blogsQueryRepository.getByIdOrNotFoundFail(blogId);
   }
 
-  /*@Get(':id')
-  async getById(@Param('id') id: string): Promise<UserViewDto> {
-    return this.usersQueryRepository.getByIdOrNotFoundFail(id);
+  @Get(":id")
+  async getById(@Param("id") id: string): Promise<BlogViewDto> {
+    return this.blogsQueryRepository.getByIdOrNotFoundFail(id);
   }
 
-  @Delete(':id')
+  @Delete(":id")
   @HttpCode(HttpStatus.NO_CONTENT)
-  async deleteUser(@Param('id') id: string): Promise<void> {
-    return this.usersService.deleteUser(id);
-  }*/
+  async deleteBlog(@Param("id") id: string): Promise<void> {
+    return this.blogsService.deleteBlog(id);
+  }
+
+  @Put(":id")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async updateBlog(
+    @Param("id") id: string,
+    @Body() body: UpdateBlogDto,
+  ): Promise<void> {
+    return this.blogsService.updateBlog(id, body);
+  }
 }

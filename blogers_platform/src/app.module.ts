2e8ecf85765eda9598e8d2f7b5a42ecb14461ce4
H4w3er <mr.gavrik378@list.ru>
2025-06-25 22:30:54 +0300

    added testing module

--- blogers_platform/src/app.module.ts
+++ blogers_platform/src/app.module.ts
@@ -4,12 +4,14 @@ import { UsersAccountsModule } from './modules/user-accounts/users-accounts.modu
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsPostsCommentsModule } from './modules/blogs-posts-comments/blogs-posts-comments.module';
+import { TestingModule } from './modules/testing/testing.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb+srv://artem:admin@data.xluig.mongodb.net/dev?retryWrites=true&w=majority&appName=Data`), // Укажите свой URL MongoDB
     UsersAccountsModule,
     BlogsPostsCommentsModule,
+    TestingModule
   ],
   controllers: [AppController],
   providers: [AppService],

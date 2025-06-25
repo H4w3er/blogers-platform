b38c8925b890d5bf1f0086f5a4e8485cfe854e51
H4w3er <mr.gavrik378@list.ru>
2025-06-25 21:12:27 +0300

    added module for blogs comments and posts

--- blogers_platform/src/app.module.ts
+++ blogers_platform/src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersAccountsModule } from './modules/user-accounts/users-accounts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogsPostsCommentsModule } from './modules/blogs-posts-comments/blogs-posts-comments.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb+srv://artem:admin@data.xluig.mongodb.net/dev?retryWrites=true&w=majority&appName=Data`), // Укажите свой URL MongoDB
     UsersAccountsModule,
+    BlogsPostsCommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],

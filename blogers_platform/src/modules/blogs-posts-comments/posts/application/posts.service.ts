d8c8d11d449e774532833cbbc3e58d5b0b22779d
H4w3er <mr.gavrik378@list.ru>
2025-06-30 15:32:50 +0300

    added method for create post

--- blogers_platform/src/modules/blogs-posts-comments/posts/application/posts.service.ts
+++ blogers_platform/src/modules/blogs-posts-comments/posts/application/posts.service.ts
@@ -1,11 +1,47 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable } from "@nestjs/common";
+import { CreatePostDto } from "../dto/create-post.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Post, PostModelType } from "../domain/post.entity";
+import { PostsRepository } from "../infrastructure/posts.repository";
+import { BlogsRepository } from "../../blogs/infrastructure/blogs.repository";
+import { BlogDocument } from "../../blogs/domain/blog.entity";
+import { Likes, LikesModelType } from "../../../../core/domain/likes.entity";
 
 @Injectable()
 export class PostsService {
   constructor(
-  ) {
+    @InjectModel(Post.name)
+    private PostModel: PostModelType,
+    private postsRepository: PostsRepository,
+    private blogsRepository: BlogsRepository,
+  ) {}
+
+  async createPost(dto: CreatePostDto): Promise<string> {
+    const blog: BlogDocument | null = await this.blogsRepository.findById(
+      dto.blogId,
+    );
+    const post = this.PostModel.createInstance({
+      title: dto.title,
+      shortDescription: dto.shortDescription,
+      content: dto.content,
+      blogId: dto.blogId,
+      blogName: blog == null ? "null" : blog.name,
+      extendedLikesInfo: {
+        likesCount: 0,
+        dislikesCount: 0,
+        myStatus: "None",
+        newestLikes: [
+          {
+            addedAt: new Date(),
+            userId: "none",
+            login: "none",
+          },
+        ],
+      },
+    });
+
+    await this.postsRepository.save(post);
+
+    return post._id.toString();
   }
-
-
 }

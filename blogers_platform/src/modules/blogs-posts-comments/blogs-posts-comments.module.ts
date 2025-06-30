d426b5060dd9c2ca214e49094858831ab19781a7
H4w3er <mr.gavrik378@list.ru>
2025-06-30 15:29:09 +0300

    added post functionality in module

--- blogers_platform/src/modules/blogs-posts-comments/blogs-posts-comments.module.ts
+++ blogers_platform/src/modules/blogs-posts-comments/blogs-posts-comments.module.ts
@@ -3,20 +3,29 @@ import { BlogsService } from './blogs/application/blogs.service';
 import { BlogsController } from './blogs/api/blogs.controller';
 import { BlogsRepository } from './blogs/infrastructure/blogs.repository';
 import { BlogsQueryRepository } from './blogs/infrastructure/blogs.query-repository';
-import { UsersAccountsModule } from '../user-accounts/users-accounts.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Blog, BlogSchema } from './blogs/domain/blog.entity';
+import { Post, PostSchema } from './posts/domain/post.entity';
+import { Likes, LikesSchema } from '../../core/domain/likes.entity';
+import { PostsController } from './posts/api/posts.controller';
+import { PostsService } from './posts/application/posts.service';
+import { PostsRepository } from './posts/infrastructure/posts.repository';
+import { PostsQueryRepository } from './posts/infrastructure/posts.query-repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
-    UsersAccountsModule
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    MongooseModule.forFeature([{ name: Likes.name, schema: LikesSchema }]),
   ],
-  controllers: [BlogsController],
+  controllers: [BlogsController, PostsController],
   providers: [
     BlogsService,
     BlogsRepository,
     BlogsQueryRepository,
+    PostsService,
+    PostsRepository,
+    PostsQueryRepository,
   ],
 })
 export class BlogsPostsCommentsModule {}

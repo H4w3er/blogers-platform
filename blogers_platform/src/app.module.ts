8fa9338c5ffe329701053cafa9a72f03253597d2
H4w3er <mr.gavrik378@list.ru>
2025-07-20 22:08:09 +0300

    some change

--- blogers_platform/src/app.module.ts
+++ blogers_platform/src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersAccountsModule } from './modules/user-accounts/users-accounts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsPostsCommentsModule } from './modules/blogs-posts-comments/blogs-posts-comments.module';
 import { TestingModule } from './modules/testing/testing.module';
+import * as process from 'node:process';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb+srv://artem:admin@data.xluig.mongodb.net/dev?retryWrites=true&w=majority&appName=Data`), // Укажите свой URL MongoDB
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL ?? 'mongodb+srv://artem:admin@data.xluig.mongodb.net/dev?retryWrites=true&w=majority&appName=Data'),
     UsersAccountsModule,
     BlogsPostsCommentsModule,
     TestingModule

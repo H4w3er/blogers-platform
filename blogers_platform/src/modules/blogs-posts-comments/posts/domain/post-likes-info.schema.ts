04507e5ca4d44ecbf0f97ace28b1f2997d7cc075
H4w3er <mr.gavrik378@list.ru>
2025-07-24 20:33:28 +0300

    created schema for ExtendedLikesInfo

---
+++ blogers_platform/src/modules/blogs-posts-comments/posts/domain/post-likes-info.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { LastLikes, LastLikesSchema } from './last-likes.entity';
+
+@Schema({
+  _id: false,
+})
+export class ExtendedLikesInfo {
+  @Prop({ type: Number, required: true, default: 0 })
+  likesCount: number;
+
+  @Prop({ type: Number, required: true, default: 0 })
+  dislikesCount: number;
+
+  @Prop({ type: String, required: true })
+  myStatus: string;
+
+  @Prop({ type: [LastLikesSchema] })
+  newestLikes: LastLikes[];
+}
+
+export const ExtendedLikesInfoSchema =
+  SchemaFactory.createForClass(ExtendedLikesInfo);

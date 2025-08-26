8ff2a2db7d994a3177cdf48eec84af77e6b5a4c3
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:30:56 +0300

    created view dto with mapper for comments path

---
+++ blogers_platform/src/modules/blogs-posts-comments/comments/api/view-dto/comments.view-dto.ts
@@ -0,0 +1,37 @@
+import { CommentDocument } from '../../domain/comment.entity';
+
+export class CommentViewDto {
+  id: string;
+  content: string;
+  commentatorInfo: {
+    userId: string,
+    userLogin: string
+  }
+  postId: string;
+  createdAt: Date;
+  likesInfo: {
+    likesCount: number;
+    dislikesCount: number;
+    myStatus: string;
+  };
+
+  static mapToView(comment: CommentDocument): CommentViewDto {
+    const dto = new CommentViewDto();
+
+    dto.id = comment._id.toString();
+    dto.content = comment.content;
+    dto.commentatorInfo = {
+      userId: comment.commentatorInfo.userId,
+      userLogin: comment.commentatorInfo.userLogin
+    };
+    dto.postId = comment.postId;
+    dto.createdAt = comment.createdAt;
+    dto.likesInfo = {
+      likesCount: comment.likesInfo.likesCount,
+      dislikesCount: comment.likesInfo.dislikesCount,
+      myStatus: comment.likesInfo.myStatus,
+    };
+    return dto;
+  }
+
+}

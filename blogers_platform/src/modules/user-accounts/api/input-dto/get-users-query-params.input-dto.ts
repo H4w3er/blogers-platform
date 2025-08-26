3154979eb1d32db5218bbfc037cb6fe66daae622
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:54:57 +0300

    added validations decorators

--- blogers_platform/src/modules/user-accounts/api/input-dto/get-users-query-params.input-dto.ts
+++ blogers_platform/src/modules/user-accounts/api/input-dto/get-users-query-params.input-dto.ts
@@ -1,8 +1,18 @@
 import { UsersSortBy } from './users-sort-by';
 import { BaseQueryParams } from '../../../../core/dto/base.query-params.input-dto';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class GetUsersQueryParams extends BaseQueryParams {
+  @IsEnum(UsersSortBy)
+  @Transform(({ value }) => value || UsersSortBy.CreatedAt)
   sortBy = UsersSortBy.CreatedAt;
+  
+  @IsOptional()
+  @IsString()
   searchLoginTerm: string | null = null;
+  
+  @IsOptional()
+  @IsString()
   searchEmailTerm: string | null = null;
 }

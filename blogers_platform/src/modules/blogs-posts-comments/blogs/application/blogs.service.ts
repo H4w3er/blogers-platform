a135b65af7db9f23d2715a42b3d0c4c430e65c19
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:27:40 +0300

    some logic changed and added two more methods

--- blogers_platform/src/modules/blogs-posts-comments/blogs/application/blogs.service.ts
+++ blogers_platform/src/modules/blogs-posts-comments/blogs/application/blogs.service.ts
@@ -1,18 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import { Blog, BlogModelType } from '../domain/blog.entity';
-import { BlogsRepository } from '../infrastructure/blogs.repository';
-import { InjectModel } from '@nestjs/mongoose';
-import { CreateBlogDto, UpdateBlogDto } from '../dto/create-blog.dto';
-
+import { Injectable } from "@nestjs/common";
+import { Blog, BlogDocument, BlogModelType } from '../domain/blog.entity';
+import { BlogsRepository } from "../infrastructure/blogs.repository";
+import { InjectModel } from "@nestjs/mongoose";
+import { CreateBlogDto, UpdateBlogDto } from "../dto/create-blog.dto";
+import {
+  CreatePostDto,
+  CreatePostForBlogDto,
+} from "../../posts/dto/create-post.dto";
+import { PostsService } from "../../posts/application/posts.service";
 
 @Injectable()
 export class BlogsService {
   constructor(
     @InjectModel(Blog.name)
     private BlogModel: BlogModelType,
-    private BlogRepository: BlogsRepository,
-  ) {
-  }
+    private blogRepository: BlogsRepository,
+    private postsService: PostsService,
+  ) {}
 
   async createBlog(dto: CreateBlogDto): Promise<string> {
     const blog = this.BlogModel.createInstance({
@@ -21,22 +25,37 @@ export class BlogsService {
       websiteUrl: dto.websiteUrl,
     });
 
-    await this.BlogRepository.save(blog);
+    await this.blogRepository.save(blog);
 
     return blog._id.toString();
   }
 
   async deleteBlog(id: string) {
-    const blog = await this.BlogRepository.findOrNotFoundFail(id);
+    const blog = await this.blogRepository.findOrNotFoundFail(id);
 
     blog.makeDeleted();
 
-    await this.BlogRepository.save(blog);
+    await this.blogRepository.save(blog);
   }
 
-  async updateBlog(id: string, dto: UpdateBlogDto){
-    const blog = await this.BlogRepository.findOrNotFoundFail(id);
-    blog.update(dto)
-    await this.BlogRepository.save(blog)
+  async updateBlog(id: string, dto: UpdateBlogDto) {
+    const blog = await this.blogRepository.findOrNotFoundFail(id);
+    blog.update(dto);
+    await this.blogRepository.save(blog);
+  }
+
+  async createPostForBlog(dto: CreatePostForBlogDto, id: string): Promise<string> {
+    const dtoForCreate: CreatePostDto = {
+      title: dto.title,
+      shortDescription: dto.shortDescription,
+      content: dto.content,
+      blogId: id,
+    };
+    await this.blogRepository.findOrNotFoundFail(id);
+    return this.postsService.createPost(dtoForCreate);
+  }
+
+  async findOrNotFindFail(blogId: string):Promise<BlogDocument> {
+    return this.blogRepository.findOrNotFoundFail(blogId)
   }
 }

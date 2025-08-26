aceab9ea0645d3fead06e15294ca262a729fabc1
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:23:33 +0300

    created class for base query params that we can accept

--- blogers_platform/src/core/dto/base.query-params.input-dto.ts
+++ blogers_platform/src/core/dto/base.query-params.input-dto.ts
@@ -1,21 +1,30 @@
 import { Type } from 'class-transformer';
+import { IsEnum, IsNumber, Min } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export enum SortDirection {
+  Asc = 'asc',
+  Desc = 'desc',
+}
 
 //базовый класс для query параметров с пагинацией
-//значения по-умолчанию применятся автоматически при настройке глобального ValidationPipe в main.ts
 export class BaseQueryParams {
   //для трансформации в number
   @Type(() => Number)
+  @IsNumber()
+  @Min(1)
   pageNumber: number = 1;
+  
   @Type(() => Number)
+  @IsNumber()
+  @Min(1)
   pageSize: number = 10;
+  
+  @IsEnum(SortDirection)
+  @Transform(({ value }) => value || SortDirection.Desc)
   sortDirection: SortDirection = SortDirection.Desc;
 
   calculateSkip() {
     return (this.pageNumber - 1) * this.pageSize;
   }
-}
-
-export enum SortDirection {
-  Asc = 'asc',
-  Desc = 'desc',
 }

8268f22ce9b99c8e7df0469f5324f38108ae5bbd
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:36:56 +0300

    added two global filters

--- blogers_platform/src/main.ts
+++ blogers_platform/src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from '@nestjs/common';
+import { DomainHttpExceptionsFilter } from './core/exceptions/filters/domain-exceptions.filter';
+import { ValidationExceptionFilter } from './core/exceptions/filters/validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
+      whitelist: true,
       transform: true,
     }),
   );
+  app.useGlobalFilters(new DomainHttpExceptionsFilter());
+  app.useGlobalFilters(new ValidationExceptionFilter());
   app.enableCors();
   app.setGlobalPrefix("api");
   await app.listen(process.env.PORT ?? 3000);

510d5371713cdbbc56ea7615fe412fd1fd5cdbb9
H4w3er <mr.gavrik378@list.ru>
2025-08-26 19:02:12 +0300

    created class meViewDto

--- blogers_platform/src/modules/user-accounts/api/view-dto/users.view-dto.ts
+++ blogers_platform/src/modules/user-accounts/api/view-dto/users.view-dto.ts
@@ -1,4 +1,5 @@
 import { UserDocument } from '../../domain/user.entity';
+import { OmitType } from '@nestjs/swagger';
 
 export class UserViewDto {
   id: string;
@@ -15,6 +16,25 @@ export class UserViewDto {
     dto.login = user.login;
     dto.id = user._id.toString();
     dto.createdAt = user.createdAt;
+
+    return dto;
+  }
+}
+
+  export class MeViewDto extends OmitType(UserViewDto, [
+    'createdAt',
+    'id',
+  ] as const) {
+  email: string;
+  login: string;
+  userId: string;
+
+  static mapToView(user: UserDocument): MeViewDto {
+    const dto = new MeViewDto();
+
+    dto.email = user.email;
+    dto.login = user.login;
+    dto.userId = user._id.toString();
 
     return dto;
   }

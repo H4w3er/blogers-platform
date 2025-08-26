90442adeace27aff0be85b345a767e2b21d7f841
H4w3er <mr.gavrik378@list.ru>
2025-08-26 19:02:30 +0300

    added imports and controllers

--- blogers_platform/src/modules/user-accounts/users-accounts.module.ts
+++ blogers_platform/src/modules/user-accounts/users-accounts.module.ts
@@ -5,17 +5,37 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './domain/user.entity';
 import { UsersRepository } from './infrastructure/users.repository';
 import { UsersQueryRepository } from './infrastructure/users.query-repository';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthQueryRepository } from './infrastructure/auth.query-repository';
+import { AuthService } from './application/auth.service';
+import { LocalStrategy } from './guards/local/local.strategy';
+import { CryptoService } from './application/crypto.service';
+import { JwtStrategy } from './guards/bearer/jwt.strategy';
+import { UsersExternalService } from './application/users-external.service';
+import { AuthController } from './api/auth.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: 'access-token-secret',
+      signOptions: { expiresIn: '5m' },
+    }),
+    NotificationsModule
   ],
-  controllers: [UsersController],
+  controllers: [UsersController, AuthController],
   providers: [
     UsersService,
     UsersRepository,
-    UsersQueryRepository
+    UsersQueryRepository,
+    AuthQueryRepository,
+    AuthService,
+    LocalStrategy,
+    CryptoService,
+    JwtStrategy,
+    UsersExternalService,
   ],
-  exports: [],
+  exports: [JwtStrategy, UsersExternalService],
 })
 export class UsersAccountsModule {}

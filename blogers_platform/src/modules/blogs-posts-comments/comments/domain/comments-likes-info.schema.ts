b6aa44d512beb764f420b6b281fd10b51b5ce84d
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:31:29 +0300

    created schema of additional likes info

---
+++ blogers_platform/src/modules/blogs-posts-comments/comments/domain/comments-likes-info.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({
+  _id: false,
+})
+export class LikesInfo {
+  @Prop({ type: Number, required: true, default: 0 })
+  likesCount: number;
+
+  @Prop({ type: Number, required: true, default: 0 })
+  dislikesCount: number;
+
+  @Prop({ type: String, required: true, default: 'none' })
+  myStatus: string;
+}
+
+export const LikesInfoSchema =
+  SchemaFactory.createForClass(LikesInfo);

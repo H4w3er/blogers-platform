a3fc648309e00442f17a5856baef155a5bba9043
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:32:35 +0300

    added validation decorators

--- blogers_platform/src/modules/blogs-posts-comments/blogs/dto/create-blog.dto.ts
+++ blogers_platform/src/modules/blogs-posts-comments/blogs/dto/create-blog.dto.ts
@@ -1,11 +1,29 @@
+import { IsString, MaxLength } from 'class-validator';
+
 export class CreateBlogDto {
+  @MaxLength(15)
+  @IsString()
   name: string;
+
+  @MaxLength(500)
+  @IsString()
   description: string;
+
+  @MaxLength(100)
+  @IsString()
   websiteUrl: string;
 }
 
 export class UpdateBlogDto {
+  @MaxLength(15)
+  @IsString()
   name: string;
+
+  @MaxLength(500)
+  @IsString()
   description: string;
+
+  @MaxLength(100)
+  @IsString()
   websiteUrl: string;
 }

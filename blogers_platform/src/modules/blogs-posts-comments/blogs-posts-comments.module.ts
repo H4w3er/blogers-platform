0ad4b6848f1663497b6b6ce67ece5cd5ab36093d
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:28:06 +0300

    added providers and module

--- blogers_platform/src/modules/blogs-posts-comments/blogs-posts-comments.module.ts
+++ blogers_platform/src/modules/blogs-posts-comments/blogs-posts-comments.module.ts
@@ -11,14 +11,20 @@ import { PostsService } from './posts/application/posts.service';
 import { PostsRepository } from './posts/infrastructure/posts.repository';
 import { PostsQueryRepository } from './posts/infrastructure/posts.query-repository';
 import { LastLikes, LastLikesSchema } from './posts/domain/last-likes.entity';
+import { Comment, CommentSchema } from './comments/domain/comment.entity';
+import { CommentsService } from './comments/application/comments.service';
+import { CommentsQueryRepository } from './comments/infrastructure/comments.query-repository';
+import { CommentsRepository } from './comments/infrastructure/comments.repository';
+import { CommentsController } from './comments/api/comments.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
     MongooseModule.forFeature([{ name: LastLikes.name, schema: LastLikesSchema }]),
+    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
   ],
-  controllers: [BlogsController, PostsController],
+  controllers: [BlogsController, PostsController, CommentsController],
   providers: [
     BlogsService,
     BlogsRepository,
@@ -26,6 +32,9 @@ import { LastLikes, LastLikesSchema } from './posts/domain/last-likes.entity';
     PostsService,
     PostsRepository,
     PostsQueryRepository,
+    CommentsService,
+    CommentsRepository,
+    CommentsQueryRepository,
   ],
 })
 export class BlogsPostsCommentsModule {}

db1c8dd3e926162223b8b1e145a9ec05fbd5e56a
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:54:46 +0300

    added validations decorators

--- blogers_platform/src/modules/blogs-posts-comments/blogs/api/input-dto/get-blogs-query-params.input-dto.ts
+++ blogers_platform/src/modules/blogs-posts-comments/blogs/api/input-dto/get-blogs-query-params.input-dto.ts
@@ -1,8 +1,15 @@
 import { BaseQueryParams } from '../../../../../core/dto/base.query-params.input-dto';
 import { BlogsSortBy } from './blogs-sort-by';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 
 export class GetBlogsQueryParams extends BaseQueryParams {
+  @IsEnum(BlogsSortBy)
+  @Transform(({ value }) => value || BlogsSortBy.CreatedAt)
   sortBy = BlogsSortBy.CreatedAt;
+
+  @IsOptional()
+  @IsString()
   searchNameTerm: string | null = null;
 }

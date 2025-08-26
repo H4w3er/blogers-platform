05714b73ff7575685e869da6a90c04b5a81bf93e
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:59:09 +0300

    added guard

--- blogers_platform/src/modules/user-accounts/api/users.controller.ts
+++ blogers_platform/src/modules/user-accounts/api/users.controller.ts
@@ -8,29 +8,35 @@ import {
   Param,
   Post,
   Query,
+  UseGuards,
 } from "@nestjs/common";
 import { UsersQueryRepository } from "../infrastructure/users.query-repository";
 import { UserViewDto } from "./view-dto/users.view-dto";
 import { UsersService } from "../application/users.service";
 import { CreateUserInputDto } from "./input-dto/users.input-dto";
-import { GetUsersQueryParams } from './input-dto/get-users-query-params.input-dto';
-import { PaginatedViewDto } from '../../../core/dto/base.paginated.view-dto';
+import { GetUsersQueryParams } from "./input-dto/get-users-query-params.input-dto";
+import { PaginatedViewDto } from "../../../core/dto/base.paginated.view-dto";
+import { BasicAuthGuard } from "../guards/basic/basic-auth.guard";
+import { Public } from "../guards/decorators/public.decorator";
 
-
-@Controller('users')
+@Controller("users")
+@UseGuards(BasicAuthGuard)
 export class UsersController {
   constructor(
     private usersQueryRepository: UsersQueryRepository,
     private usersService: UsersService,
   ) {}
 
-  @Get(':id')
-  async getById(@Param('id') id: string): Promise<UserViewDto> {
+  @Get(":id")
+  async getById(@Param("id") id: string): Promise<UserViewDto> {
     return this.usersQueryRepository.getByIdOrNotFoundFail(id);
   }
 
+  @Public()
   @Get()
-  async getAll(@Query() query: GetUsersQueryParams): Promise<PaginatedViewDto<UserViewDto[]>> {
+  async getAll(
+    @Query() query: GetUsersQueryParams,
+  ): Promise<PaginatedViewDto<UserViewDto[]>> {
     return this.usersQueryRepository.getAll(query);
   }
 
@@ -41,9 +47,9 @@ export class UsersController {
     return this.usersQueryRepository.getByIdOrNotFoundFail(userId);
   }
 
-  @Delete(':id')
+  @Delete(":id")
   @HttpCode(HttpStatus.NO_CONTENT)
-  async deleteUser(@Param('id') id: string): Promise<void> {
+  async deleteUser(@Param("id") id: string): Promise<void> {
     return this.usersService.deleteUser(id);
   }
 }

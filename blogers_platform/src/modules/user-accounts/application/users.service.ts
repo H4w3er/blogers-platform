08492ca23480d125eedc8aac84270717f4725b58
H4w3er <mr.gavrik378@list.ru>
2025-08-26 19:01:52 +0300

    added some methods for working with email and some else

--- blogers_platform/src/modules/user-accounts/application/users.service.ts
+++ blogers_platform/src/modules/user-accounts/application/users.service.ts
@@ -1,22 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User, UserModelType } from '../domain/user.entity';
-import { CreateUserDto, UpdateUserDto } from '../dto/create-user.dto';
-import * as bcrypt from 'bcrypt';
-import { UsersRepository } from '../infrastructure/users.repository';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { User, UserModelType } from "../domain/user.entity";
+import { CreateUserDto, UpdateUserDto } from "../dto/create-user.dto";
+import { UsersRepository } from "../infrastructure/users.repository";
+import { Types } from "mongoose";
+import { CryptoService } from "./crypto.service";
+import { EmailService } from "../../notifications/email.service";
+import { v4 as uuidv4 } from "uuid";
+import {
+  DomainException,
+  Extension,
+} from "../../../core/exceptions/domain-exceptions";
+import { DomainExceptionCode } from "../../../core/exceptions/domain-exception-codes";
+import { NewPasswordDto } from "../api/input-dto/auth.input-dto";
+import { CreateUserInputDto } from '../api/input-dto/users.input-dto';
 
 @Injectable()
 export class UsersService {
   constructor(
-    //инжектирование модели в сервис через DI
     @InjectModel(User.name)
     private UserModel: UserModelType,
     private usersRepository: UsersRepository,
+    private cryptoService: CryptoService,
+    private emailService: EmailService,
   ) {}
 
   async createUser(dto: CreateUserDto): Promise<string> {
+    const userWithTheSameLogin = await this.usersRepository.findByLogin(
+      dto.login,
+    );
+    if (!!userWithTheSameLogin) {
+      throw new BadRequestException({
+        code: 400,
+        message: ["Login is not unique"],
+      });
+    }
 
-    const passwordHash = await bcrypt.hash(dto.password, 10);
+    const userWithTheSameEmail = await this.usersRepository.findByEmail(
+      dto.email,
+    );
+    if (!!userWithTheSameEmail) {
+      throw new BadRequestException({
+        code: 400,
+        message: ["Email is not unique"],
+      });
+    }
+
+    const passwordHash = await this.cryptoService.createPasswordHash(
+      dto.password,
+    );
 
     const user = this.UserModel.createInstance({
       email: dto.email,
@@ -28,12 +60,11 @@ export class UsersService {
 
     return user._id.toString();
   }
+
   async updateUser(id: string, dto: UpdateUserDto): Promise<string> {
     const user = await this.usersRepository.findOrNotFoundFail(id);
 
-    // не присваиваем св-ва сущностям напрямую в сервисах! даже для изменения одного св-ва
-    // создаём метод
-    user.update(dto); // change detection
+    user.update(dto);
 
     await this.usersRepository.save(user);
 
@@ -46,5 +77,92 @@ export class UsersService {
     user.makeDeleted();
 
     await this.usersRepository.save(user);
+  }
+
+  async registerUser(dto: CreateUserInputDto) {
+    const createdUserId = await this.createUser(dto);
+
+    const confirmCode = uuidv4();
+    const user = await this.usersRepository.findOrNotFoundFail(
+      new Types.ObjectId(createdUserId),
+    );
+
+    user.confirmationCode = confirmCode;
+    await this.usersRepository.save(user);
+
+    this.emailService
+      .sendConfirmationEmail(user.email, confirmCode)
+      .catch(console.error);
+  }
+
+  async confirmEmail(code: string) {
+    const user = await this.usersRepository.findByCode(code);
+    if (!user || user.isEmailConfirmed)
+      throw new BadRequestException({
+        message: ["Code is invalid"],
+      });
+
+    user.setConfirmation();
+    await this.usersRepository.save(user);
+  }
+
+  async emailRecovery(email: string) {
+    const recoveryCode = uuidv4();
+
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      this.emailService
+        .sendConfirmationEmail(email, recoveryCode)
+        .catch(console.error);
+    } else {
+      user.recoveryCode = recoveryCode;
+      await this.usersRepository.save(user);
+      this.emailService
+        .sendConfirmationEmail(email, recoveryCode)
+        .catch(console.error);
+    }
+  }
+
+  async newPassword(dto: NewPasswordDto) {
+    const user = await this.usersRepository.findByRecoveryCode(
+      dto.recoveryCode,
+    );
+
+    if (!user)
+      throw new DomainException({
+        code: DomainExceptionCode.BadRequest,
+        message: "user not found",
+      });
+
+    user.passwordHash = await this.cryptoService.createPasswordHash(
+      dto.newPassword,
+    );
+
+    await this.usersRepository.save(user);
+  }
+
+  async emailResending(email: string) {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user)
+      throw new BadRequestException({
+        message: ["Email not found"],
+      });
+
+    if (user.isEmailConfirmed){
+      throw new BadRequestException({
+        message: ['Email already confirmed']
+      })
+    }
+
+    const confirmCode = uuidv4();
+
+    user.confirmationCode = confirmCode;
+    await this.usersRepository.save(user);
+
+    this.emailService
+      .sendConfirmationEmail(email, confirmCode)
+      .catch(console.error);
   }
 }

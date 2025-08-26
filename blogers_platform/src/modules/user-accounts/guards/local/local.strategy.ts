66e466eadcf005af1c12f46eaa6cbf07adfd26fa
H4w3er <mr.gavrik378@list.ru>
2025-08-26 18:53:40 +0300

    created local strategy

---
+++ blogers_platform/src/modules/user-accounts/guards/local/local.strategy.ts
@@ -0,0 +1,26 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from '../../application/auth.service';
+import { DomainException } from '../../../../core/exceptions/domain-exceptions';
+import { UserContextDto } from '../dto/user-context.dto';
+import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'loginOrEmail' });
+  }
+
+  async validate(username: string, password: string): Promise<UserContextDto> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        message: 'Invalid username or password',
+      });
+    }
+
+    return user;
+  }
+}

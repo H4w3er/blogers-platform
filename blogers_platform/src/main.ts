2349c1d4575a7bbb94770e8652a9df63d004993a
H4w3er <mr.gavrik378@list.ru>
2025-08-27 23:36:19 +0300

    added cookieParser

--- blogers_platform/src/main.ts
+++ blogers_platform/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from '@nestjs/common';
 import { DomainHttpExceptionsFilter } from './core/exceptions/filters/domain-exceptions.filter';
 import { ValidationExceptionFilter } from './core/exceptions/filters/validation-error.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new DomainHttpExceptionsFilter());
   app.useGlobalFilters(new ValidationExceptionFilter());
+  app.use(cookieParser())
   app.enableCors();
   app.setGlobalPrefix("api");
   await app.listen(process.env.PORT ?? 3000);

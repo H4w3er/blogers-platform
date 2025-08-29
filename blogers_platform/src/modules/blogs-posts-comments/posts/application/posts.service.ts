6b5c28dc85173954087ab5acf4ae40a6057bf092
H4w3er <mr.gavrik378@list.ru>
2025-08-29 12:56:33 +0300

    added method for update likes count to post

--- blogers_platform/src/modules/blogs-posts-comments/posts/application/posts.service.ts
+++ blogers_platform/src/modules/blogs-posts-comments/posts/application/posts.service.ts
@@ -5,6 +5,7 @@ import { Post, PostModelType } from "../domain/post.entity";
 import { PostsRepository } from "../infrastructure/posts.repository";
 import { BlogsRepository } from "../../blogs/infrastructure/blogs.repository";
 import { BlogDocument } from "../../blogs/domain/blog.entity";
+import { UsersRepository } from '../../../user-accounts/infrastructure/users.repository';
 
 @Injectable()
 export class PostsService {
@@ -13,6 +14,7 @@ export class PostsService {
     private PostModel: PostModelType,
     private postsRepository: PostsRepository,
     private blogsRepository: BlogsRepository,
+    private usersRepository: UsersRepository,
   ) {}
 
   async createPost(dto: CreatePostDto): Promise<string> {
@@ -56,5 +58,14 @@ export class PostsService {
     post.makeDeleted();
 
     await this.postsRepository.save(post);
+  }
+
+  async likeUnlikePost(likeStatus: string, postId: string, likerId: string){
+    const post = this.postsRepository.findOrNotFoundFail(postId)
+    const user = this.usersRepository.findOrNotFoundFail(likerId)
+
+    if (likeStatus == 'like') {
+
+    }
   }
 }

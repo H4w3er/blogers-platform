e95f6465c91a09108b6a397d5e46b035cdfe131e
H4w3er <mr.gavrik378@list.ru>
2025-08-29 12:55:52 +0300

    added two endpoints for work with comments and likes

--- blogers_platform/src/modules/blogs-posts-comments/posts/api/posts.controller.ts
+++ blogers_platform/src/modules/blogs-posts-comments/posts/api/posts.controller.ts
@@ -8,30 +8,38 @@ import {
   Param,
   Post,
   Put,
-  Query, UseGuards,
-} from '@nestjs/common';
+  Query,
+  UseGuards,
+} from "@nestjs/common";
 import { GetPostsQueryParams } from "./input-dto/get-posts-query-params.input-dto";
 import { PaginatedViewDto } from "../../../../core/dto/base.paginated.view-dto";
 import { PostsQueryRepository } from "../infrastructure/posts.query-repository";
 import { PostViewDto } from "./view-dto/posts.view-dto";
-import { CreatePostDto, UpdatePostDto } from '../dto/create-post.dto';
+import { CreatePostDto, UpdatePostDto } from "../dto/create-post.dto";
 import { PostsService } from "../application/posts.service";
-import { CommentViewDto } from '../../comments/api/view-dto/comments.view-dto';
-import { CommentsQueryRepository } from '../../comments/infrastructure/comments.query-repository';
-import { GetCommentsQueryParams } from '../../comments/api/input-dto/get-comments-query-params.input-dto';
+import { CommentViewDto } from "../../comments/api/view-dto/comments.view-dto";
+import { CommentsQueryRepository } from "../../comments/infrastructure/comments.query-repository";
+import { GetCommentsQueryParams } from "../../comments/api/input-dto/get-comments-query-params.input-dto";
+import { Public } from "../../../user-accounts/guards/decorators/public.decorator";
+import { BasicAuthGuard } from "../../../user-accounts/guards/basic/basic-auth.guard";
+import { CreateCommentDto } from "../../comments/dto/create-comment.dto";
+import { CommentsService } from "../../comments/application/comments.service";
+import {
+  ExtractUserFromRequest
+} from '../../../user-accounts/guards/decorators/param/extract-user-from-request.decorator';
+import { UserContextDto } from '../../../user-accounts/guards/dto/user-context.dto';
 import { JwtAuthGuard } from '../../../user-accounts/guards/bearer/jwt-auth.guard';
-import { Public } from '../../../user-accounts/guards/decorators/public.decorator';
+import { LikeStatusDto } from '../dto/like-status.dto';
 
 @Controller("posts")
-@UseGuards(JwtAuthGuard)
 export class PostsController {
   constructor(
     private postsQueryRepository: PostsQueryRepository,
     private postsService: PostsService,
-    private commentsQueryRepository: CommentsQueryRepository
+    private commentsQueryRepository: CommentsQueryRepository,
+    private commentsService: CommentsService,
   ) {}
 
-  @Public()
   @Get()
   async getAll(
     @Query() query: GetPostsQueryParams,
@@ -39,6 +47,7 @@ export class PostsController {
     return this.postsQueryRepository.getAll(query);
   }
 
+  @UseGuards(BasicAuthGuard)
   @Post()
   async createPost(@Body() body: CreatePostDto): Promise<PostViewDto> {
     const postId = await this.postsService.createPost(body);
@@ -51,6 +60,7 @@ export class PostsController {
     return this.postsQueryRepository.getByIdOrNotFoundFail(id);
   }
 
+  @UseGuards(BasicAuthGuard)
   @Put(":id")
   @HttpCode(HttpStatus.NO_CONTENT)
   async updatePost(
@@ -60,6 +70,7 @@ export class PostsController {
     return this.postsService.updatePost(id, body);
   }
 
+  @UseGuards(BasicAuthGuard)
   @Delete(":id")
   @HttpCode(HttpStatus.NO_CONTENT)
   async deletePost(@Param("id") id: string): Promise<void> {
@@ -68,7 +79,33 @@ export class PostsController {
 
   @Public()
   @Get(":id/comments")
-  async getCommentsForPost(@Query() query: GetCommentsQueryParams, @Param("id") id: string): Promise<PaginatedViewDto<CommentViewDto[]>> {
+  async getCommentsForPost(
+    @Query() query: GetCommentsQueryParams,
+    @Param("id") id: string,
+  ): Promise<PaginatedViewDto<CommentViewDto[]>> {
     return this.commentsQueryRepository.getAll(query, id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post(":id/comments")
+  async createCommentForPost(
+    @Param("id") postId: string,
+    @Body() body: CreateCommentDto,
+    @ExtractUserFromRequest() user: UserContextDto
+  ): Promise<CommentViewDto> {
+    const commentId = await this.commentsService.createComment(body, postId, user.id);
+    return this.commentsQueryRepository.getByIdOrNotFoundFail(commentId);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtAuthGuard)
+  @Put(":id/like-status")
+  async updateLikeStatus(
+    @Param("id") postId: string,
+    @Body() body: LikeStatusDto,
+    @ExtractUserFromRequest() user: UserContextDto
+  ): Promise<> {
+    const like = await this.postsService.likeUnlikePost(body.likeStatus, postId, user.id);
+
   }
 }

0111be651839bad4f2acea8963290a7e2a2a6402
H4w3er <mr.gavrik378@list.ru>
2025-08-29 12:54:29 +0300

    added method of creating instance

--- blogers_platform/src/modules/blogs-posts-comments/posts/domain/last-likes.entity.ts
+++ blogers_platform/src/modules/blogs-posts-comments/posts/domain/last-likes.entity.ts
@@ -1,22 +1,31 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument, Model } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument, Model } from "mongoose";
+import { CreateLikesDomainDto } from "./dto/create-likesInfo.domain.dto";
 
 @Schema({
   _id: false,
 })
 export class LastLikes {
-  @Prop({ type: String, required: true})
+  @Prop({ type: String, required: true })
   addedAt: string;
 
-  @Prop({ type: String, required: true})
+  @Prop({ type: String, required: true })
   userId: string;
 
-  @Prop({ type: String, required: true})
+  @Prop({ type: String, required: true })
   login: string;
+
+  static createInstance(dto: CreateLikesDomainDto): LastLikesDocument {
+    const likeInfo = new this();
+    likeInfo.addedAt = new Date().toISOString();
+    likeInfo.userId = dto.userId;
+    likeInfo.login = dto.login;
+
+    return likeInfo as LastLikesDocument;
+  }
 }
 
-export const LastLikesSchema =
-  SchemaFactory.createForClass(LastLikes);
+export const LastLikesSchema = SchemaFactory.createForClass(LastLikes);
 
 LastLikesSchema.loadClass(LastLikes);
 

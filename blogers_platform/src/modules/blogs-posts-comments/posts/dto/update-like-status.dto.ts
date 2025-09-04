36d7d1f293b3ab71d8b14871e1a247fabddc6b52
H4w3er <mr.gavrik378@list.ru>
2025-09-04 22:08:37 +0300

    changed name of property

--- blogers_platform/src/modules/blogs-posts-comments/posts/dto/update-like-status.dto.ts
+++ blogers_platform/src/modules/blogs-posts-comments/posts/dto/update-like-status.dto.ts
@@ -3,5 +3,5 @@ export type LikeStatus = 'Like' | 'Dislike' | 'None';
 export interface LikeStatusUpdateDto {
     newLikeStatus: LikeStatus;
     userId: string;
-    postId: string;
+    postOrCommentId: string;
 }

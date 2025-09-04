8456e609aec81efb101cc2599a1448c42245a03b
H4w3er <mr.gavrik378@list.ru>
2025-09-04 22:08:03 +0300

    finished logic for update likeStatus

--- blogers_platform/src/modules/blogs-posts-comments/posts/application/usecases/update-like-status.usecase.ts
+++ blogers_platform/src/modules/blogs-posts-comments/posts/application/usecases/update-like-status.usecase.ts
@@ -1,68 +1,90 @@
-import {PostsRepository} from "../../infrastructure/posts.repository";
-import {LikeStatusUpdateDto} from "../../dto/update-like-status.dto";
-import {CommandHandler, ICommandHandler} from "@nestjs/cqrs";
-import {UserStatusesModelType} from "../../domain/user-statuses.entity";
-import {UserModelType} from "../../../../user-accounts/domain/user.entity";
-import {NotFoundException} from "@nestjs/common";
+import { PostsRepository } from '../../infrastructure/posts.repository';
+import { LikeStatusUpdateDto } from '../../dto/update-like-status.dto';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { UserStatuses, UserStatusesModelType } from '../../domain/user-statuses.entity';
+import {
+  User,
+  UserModelType,
+} from '../../../../user-accounts/domain/user.entity';
+import { NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostModelType } from '../../domain/post.entity';
+import { Comment, CommentModelType } from '../../../comments/domain/comment.entity';
 
 export class UpdateLikeStatusCommand {
-    constructor(
-        public dto: LikeStatusUpdateDto
-    ) {
-    }
+  constructor(public dto: LikeStatusUpdateDto) {
+  }
 }
 
 @CommandHandler(UpdateLikeStatusCommand)
 export class UpdateLikeStatusUseCase
-    implements ICommandHandler<UpdateLikeStatusCommand> {
-    constructor(
-        private postsRepository: PostsRepository,
-        private UserStatusesModel: UserStatusesModelType,
-        private UserModel: UserModelType,
-    ) {
+  implements ICommandHandler<UpdateLikeStatusCommand, void> {
+  constructor(
+    @InjectModel(User.name)
+    private UserModel: UserModelType,
+    private postsRepository: PostsRepository,
+    @InjectModel(UserStatuses.name)
+    private UserStatusesModel: UserStatusesModelType,
+    @InjectModel(Post.name)
+    private PostModel: PostModelType,
+    @InjectModel(Comment.name)
+    private CommentModel: CommentModelType,
+  ) {
+  }
+
+  async execute({ dto }: UpdateLikeStatusCommand): Promise<void> {
+    const { newLikeStatus, userId, postOrCommentId } = dto;
+
+    const post = await this.PostModel.findOne({ _id: postOrCommentId });
+    const comment = await this.CommentModel.findOne({ _id: postOrCommentId });
+    if (!post && !comment)
+      throw new NotFoundException({
+        message: "not found",
+      });
+
+    let oldStatus = 'None';
+    const userStatus = await this.UserStatusesModel.findOne({
+      userId: userId,
+      postOrCommentId: postOrCommentId,
+    });
+    if (!!userStatus) oldStatus = userStatus.userStatus;
+    const user = await this.UserModel.findOne({ _id: userId });
+    if (!user) throw new NotFoundException();
+
+    const userLogin = user.login;
+    await this.executeStatusChangeOperation(
+      newLikeStatus,
+      oldStatus,
+      userId,
+      userLogin,
+      postOrCommentId,
+    );
+  }
+
+  private async executeStatusChangeOperation(
+    newStatus: string,
+    oldStatus: string,
+    userId: string,
+    userLogin: string,
+    postOrCommentId: string,
+  ): Promise<void> {
+    const operationKey = `${oldStatus}-${newStatus}`;
+    const operations: Record<string, () => Promise<void>> = {
+      'None-Like': () =>
+        this.postsRepository.addLikeToPost(postOrCommentId, userId, userLogin),
+      'None-Dislike': () => this.postsRepository.addDislikeToPost(postOrCommentId, userId, userLogin),
+      'Like-None': () =>
+        this.postsRepository.removeLikeToPost(postOrCommentId, userId, userLogin),
+      'Dislike-None': () => this.postsRepository.removeDislikeToPost(postOrCommentId, userId, userLogin),
+      'Dislike-Like': () =>
+        this.postsRepository.switchDislikeToLike(postOrCommentId, userId, userLogin),
+      'Like-Dislike': () =>
+        this.postsRepository.switchLikeToDislike(postOrCommentId, userId, userLogin),
+    };
+
+    const operation = operations[operationKey];
+    if (operation) {
+      await operation();
     }
-
-    async execute(dto: LikeStatusUpdateDto): Promise<void> {
-        const {newLikeStatus, userId, postId} = dto
-        let oldStatus = 'none'
-        const userStatus = await this.UserStatusesModel.findOne({userId: userId, postId: postId})
-        const user = await this.UserModel.findOne({userId: userId})
-        if (!user) throw new NotFoundException()
-
-        const userLogin = user.login
-
-        if (!!userStatus) oldStatus = userStatus.userStatus
-
-        await this.executeStatusChangeOperation(newLikeStatus, oldStatus, userId, userLogin, postId);
-    }
-
-    private async executeStatusChangeOperation(
-        newStatus: string,
-        oldStatus: string,
-        userId: string,
-        userLogin: string,
-        postId: string
-    ): Promise<void> {
-        const operationKey = `${oldStatus}-${newStatus}`;
-
-        const operations = {
-            'None-Like': () => this.postsRepository.addLikeToPost(postId, userId, userLogin),
-            'None-Dislike': () => this.postsRepository.addDislikeToPost(postId),
-            'Like-None': () => this.postsRepository.removeLikeToPost(postId, userId, userLogin),
-            'Dislike-None': () => this.postsRepository.removeDislikeToPost(postId),
-            'Dislike-Like': () => {
-                this.postsRepository.removeDislikeToPost(postId);
-                this.postsRepository.addLikeToPost(postId, userId, userLogin)
-            },
-            'Like-Dislike': () => {
-                this.postsRepository.removeLikeToPost(postId, userId, userLogin);
-                this.postsRepository.addDislikeToPost(postId)
-            },
-        };
-
-        const operation = operations[operationKey];
-        if (operation) {
-            await operation();
-        }
-    }
+  }
 }

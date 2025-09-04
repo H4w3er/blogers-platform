a229fdb75dd973b086578034287dd298929ea524
H4w3er <mr.gavrik378@list.ru>
2025-09-04 22:05:12 +0300

    added endpoints with methods updateCommentById and updateCommentLikeStatus

--- blogers_platform/src/modules/blogs-posts-comments/comments/api/comments.controller.ts
+++ blogers_platform/src/modules/blogs-posts-comments/comments/api/comments.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Put, Query, UseGuards } from '@nestjs/common';
 import { GetCommentsQueryParams } from "./input-dto/get-comments-query-params.input-dto";
 import { CommentViewDto } from "./view-dto/comments.view-dto";
 import { PaginatedViewDto } from "../../../../core/dto/base.paginated.view-dto";
 import { CommentsQueryRepository } from "../infrastructure/comments.query-repository";
 import { CommentsService } from '../application/comments.service';
+import { UpdateCommentInputDto } from './input-dto/create-comment.input-dto';
+import { JwtAuthGuard } from '../../../user-accounts/guards/bearer/jwt-auth.guard';
+import {
+  ExtractUserFromRequest
+} from '../../../user-accounts/guards/decorators/param/extract-user-from-request.decorator';
+import { UserContextDto } from '../../../user-accounts/guards/dto/user-context.dto';
+import { LikeStatusInputDto } from '../../posts/dto/like-status.dto';
+import { UpdateLikeStatusCommand } from '../../posts/application/usecases/update-like-status.usecase';
+import { CommandBus } from '@nestjs/cqrs';
 
 @Controller("comments")
 export class CommentsController {
   constructor(
     private commentsQueryRepository: CommentsQueryRepository,
-    private commentService: CommentsService
+    private commentService: CommentsService,
+    private readonly commandBus: CommandBus,
   ) {}
 
   @Get()
@@ -22,5 +32,19 @@ export class CommentsController {
   @Get(":id")
   async getById(@Param("id") id: string): Promise<CommentViewDto> {
     return this.commentsQueryRepository.getByIdOrNotFoundFail(id);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtAuthGuard)
+  @Put(":id")
+  async updateCommentById(@Param("id") id: string, @Body() body: UpdateCommentInputDto, @ExtractUserFromRequest() user: UserContextDto) {
+    await this.commentService.updateComment(id, body, user.id);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtAuthGuard)
+  @Put(":id/like-status")
+  async updateCommentLikeStatus(@Param("id") commentId: string, @Body() body: LikeStatusInputDto, @ExtractUserFromRequest() user: UserContextDto) {
+    await this.commandBus.execute<UpdateLikeStatusCommand>(new UpdateLikeStatusCommand({newLikeStatus: body.likeStatus, userId: user.id, postOrCommentId: commentId}))
   }
 }

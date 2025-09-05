b7f8a76ff026306797c94b449b559412965572af
H4w3er <mr.gavrik378@list.ru>
2025-09-05 20:34:11 +0300

    changed logic of return

--- blogers_platform/src/modules/user-accounts/guards/bearer/jwt-optional-auth.guard.ts
+++ blogers_platform/src/modules/user-accounts/guards/bearer/jwt-optional-auth.guard.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtOptionalAuthGuard extends AuthGuard('optional-jwt') {
+export class JwtOptionalAuthGuard extends AuthGuard('jwt') {
   handleRequest(err, user) {
-    if (!user) {
-      user.id = ''
+    if (err || !user) {
+      return null;
     }
     return user;
   }

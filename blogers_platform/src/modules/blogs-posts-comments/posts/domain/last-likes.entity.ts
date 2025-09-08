19826cc18a7420ff80cd43ec7461b3bc103d0ba3
Artem <mr.gavrik378@list.ru>
2025-09-09 00:22:33 +0300

    added prop isDeleted to check is it deleted

--- blogers_platform/src/modules/blogs-posts-comments/posts/domain/last-likes.entity.ts
+++ blogers_platform/src/modules/blogs-posts-comments/posts/domain/last-likes.entity.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument, Model } from "mongoose";
-import { CreateLikesDomainDto } from "./dto/create-likesInfo.domain.dto";
+import {Error, HydratedDocument, Model} from "mongoose";
+import { CreateLikesDomainDto } from "../../likes/dto/create-likesInfo.domain.dto";
 
 @Schema()
 export class LastLikes {
@@ -16,14 +16,24 @@ export class LastLikes {
   @Prop({ type: String, required: true })
   login: string;
 
+  @Prop({type: Boolean, required: true})
+  isDeleted: boolean;
+
   static createInstance(dto: CreateLikesDomainDto): LastLikesDocument {
     const likeInfo = new this();
     likeInfo.addedAt = new Date().toISOString();
     likeInfo.userId = dto.userId;
     likeInfo.postId = dto.postId;
     likeInfo.login = dto.login;
-
+    likeInfo.isDeleted = false;
     return likeInfo as LastLikesDocument;
+  }
+
+  makeDeleted(){
+    if (this.isDeleted) {
+      throw new Error("Entity already deleted");
+    }
+    this.isDeleted = true;
   }
 }
 

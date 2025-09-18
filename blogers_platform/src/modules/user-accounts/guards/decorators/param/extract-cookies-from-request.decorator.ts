25f85d5824b44a1dd11157c7478d53710babb998
H4w3er <mr.gavrik378@list.ru>
2025-09-18 10:59:47 +0300

    added ip and title in return

--- blogers_platform/src/modules/user-accounts/guards/decorators/param/extract-cookies-from-request.decorator.ts
+++ blogers_platform/src/modules/user-accounts/guards/decorators/param/extract-cookies-from-request.decorator.ts
@@ -1,6 +1,12 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
-export const ExtractCookieFromRequest = createParamDecorator((data: string, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return data ? request.cookies?.[data] : request.cookies;
-});
+export const ExtractCookieFromRequest = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return {
+      refreshToken: data ? request.cookies?.[data] : request.cookies,
+      ip: request.headers['user-agent'],
+      title: request.headers['x-forwarded-for'],
+    };
+  },
+);

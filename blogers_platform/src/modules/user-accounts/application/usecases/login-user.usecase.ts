6c5e0b57948e693307fa8eb45946289392536920
H4w3er <mr.gavrik378@list.ru>
2025-09-18 11:00:21 +0300

    added sessions create

--- blogers_platform/src/modules/user-accounts/application/usecases/login-user.usecase.ts
+++ blogers_platform/src/modules/user-accounts/application/usecases/login-user.usecase.ts
@@ -1,34 +1,55 @@
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
-
 import { JwtService } from "@nestjs/jwt";
 import { UserContextDto } from "../../guards/dto/user-context.dto";
+import { ExtractCookiesContextDto } from "../../guards/dto/extract-cookies.context.dto";
+import { Session, SessionModelType } from '../../domain/session.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { v4 as uuidv4 } from 'uuid';
 
 export class LoginUserCommand {
-  constructor(public dto: UserContextDto) {}
+  constructor(
+    public userDto: UserContextDto,
+    public sessionDto: ExtractCookiesContextDto,
+  ) {}
 }
 
 @CommandHandler(LoginUserCommand)
 export class LoginUserUseCase implements ICommandHandler<LoginUserCommand> {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    @InjectModel(Session.name)
+    private sessionModel: SessionModelType,
+  ) {}
 
-  async execute({ dto }: LoginUserCommand): Promise<{
+  async execute(dto: LoginUserCommand): Promise<{
     accessToken: string;
     refreshToken: string;
   }> {
+    const deviceId = uuidv4()
+
     const accessToken = this.jwtService.sign(
-      { id: dto.id } as UserContextDto,
+      { id: dto.userDto.id } as UserContextDto,
       { expiresIn: "10m" },
     );
     const refreshToken = this.jwtService.sign(
       {
-        id: dto.id,
-        deviceId: "deviceId",
+        id: dto.userDto.id,
+        deviceId: deviceId,
       } as UserContextDto,
-      { 
+      {
         expiresIn: "24h",
-        secret: 'refresh-token-secret'
+        secret: "refresh-token-secret",
       },
     );
+
+    const session = this.sessionModel.createInstance({
+      ip: dto.sessionDto.ip,
+      title: dto.sessionDto.title,
+      deviceId: deviceId,
+      userId: dto.userDto.id,
+    });
+    await session.save()
+
     return {
       accessToken,
       refreshToken,

1c39f45dcc63213e63fbb3d4e2c4d1d2f01f4df0
H4w3er <mr.gavrik378@list.ru>
2025-09-18 11:02:24 +0300

    added session delete command

--- blogers_platform/src/modules/user-accounts/application/usecases/logout-user.usecase.ts
+++ blogers_platform/src/modules/user-accounts/application/usecases/logout-user.usecase.ts
@@ -1,25 +1,39 @@
-import {CommandHandler, ICommandHandler} from "@nestjs/cqrs";
-import {JwtService} from "@nestjs/jwt";
-import {UsersRepository} from "../../infrastructure/users.repository";
-import {NotFoundException, UnauthorizedException} from "@nestjs/common";
-import {UserContextDto} from "../../guards/dto/user-context.dto";
+import { CommandBus, CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { JwtService } from '@nestjs/jwt';
+import { UsersRepository } from '../../infrastructure/users.repository';
+import { UnauthorizedException } from '@nestjs/common';
+import { UserContextDto } from '../../guards/dto/user-context.dto';
+import { ExtractCookiesContextDto } from '../../guards/dto/extract-cookies.context.dto';
+import { DeleteSessionCommand } from './delete-session.usecase';
 
 export class LogoutUserCommand {
-    constructor(public refreshToken: string, public userDto: UserContextDto) {
-    }
+  constructor(
+    public cookiesDto: ExtractCookiesContextDto,
+    public userDto: UserContextDto,
+  ) {
+  }
 }
 
 @CommandHandler(LogoutUserCommand)
 export class LogoutUserUseCase implements ICommandHandler<LogoutUserCommand> {
-    constructor(private jwtService: JwtService, private usersRepository: UsersRepository) {
-    }
+  constructor(
+    private jwtService: JwtService,
+    private usersRepository: UsersRepository,
+    private commandBus: CommandBus,
+  ) {
+  }
 
-    async execute( dto : LogoutUserCommand): Promise<void> {
-        const user = await this.usersRepository.findById(dto.userDto.id)
-        if (!user || user?.refreshTokenBlackList.includes(dto.refreshToken)){
-            throw new UnauthorizedException()
-        }
-        await this.usersRepository.addToBlackList(dto.refreshToken, dto.userDto.id)
-        await this.usersRepository.save(user)
+  async execute(dto: LogoutUserCommand): Promise<void> {
+    const user = await this.usersRepository.findById(dto.userDto.id);
+    if (!user || user?.refreshTokenBlackList.includes(dto.cookiesDto.refreshToken)) {
+      throw new UnauthorizedException();
     }
+    await this.usersRepository.addToBlackList(dto.cookiesDto.refreshToken, dto.userDto.id);
+    await this.usersRepository.save(user);
+
+    await this.commandBus.execute(new DeleteSessionCommand({
+      deviceId: dto.cookiesDto.refreshToken,
+      userId: dto.userDto.id
+    }));
+  }
 }
